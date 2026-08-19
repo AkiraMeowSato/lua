b13646fdbe94a8318bc3366e64f3a2346a854159
Akira <103586357+AkiraMeowSato@users.noreply.github.com>
2026-08-18 20:36:19 -0700

    Update version number to 1.1.0

--- menu.lua
+++ menu.lua
@@ -4,7 +4,7 @@
 -- =========================================================================
 
 local CustomLib = {
-    Version = "1.0.0",
+    Version = "1.1.0",
     Author = "Custom",
     Title = "FATALITY",
     Defaults = {}
