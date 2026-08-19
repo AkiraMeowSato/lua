65b4fd18c3465c5533d03e185aff115b2a44a74e
Akira <103586357+AkiraMeowSato@users.noreply.github.com>
2026-08-18 20:55:34 -0700

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -35,7 +35,8 @@ local AHHubLib = {
     ActiveDrawings = {},
     ActiveHighlights = {},
     Notifications = {},
-    Unloaded = false
+    Unloaded = false,
+    Diagnostics = {} -- Properly initialized table
 }
 
 -- =========================================================================
